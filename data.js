b542a2d0accf29c6d38fca070397b3a17dc9c712
davlatfarruxov <76444252+davlatfarruxov@users.noreply.github.com>
2025-11-10 17:23:14 +0500

    commit

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-reception-area",
-      "name": "Reception Area",
+      "id": "0-safi-street-view",
+      "name": "Safi (street view)",
       "levels": [
         {
           "tileSize": 256,
@@ -28,23 +28,23 @@ var APP_DATA = {
       ],
       "faceSize": 2976,
       "initialViewParameters": {
-        "yaw": -1.2031919201596484,
-        "pitch": 0.0009650567763230811,
-        "fov": 1.3926760049349705
+        "yaw": 0.22309019448452005,
+        "pitch": 0.19130305270716974,
+        "fov": 1.325599857056214
       },
       "linkHotspots": [
         {
-          "yaw": -2.9938766402200265,
-          "pitch": 0.10580413922761167,
+          "yaw": 0.0132188957628383,
+          "pitch": 0.8343593148469512,
           "rotation": 0,
-          "target": "1-main-showroom"
+          "target": "1-safi-1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-main-showroom",
-      "name": "Main Showroom",
+      "id": "1-safi-1",
+      "name": "Safi 1",
       "levels": [
         {
           "tileSize": 256,
@@ -70,29 +70,29 @@ var APP_DATA = {
       ],
       "faceSize": 2976,
       "initialViewParameters": {
-        "yaw": 0.0067128048153630715,
-        "pitch": 0.02404354324589164,
-        "fov": 1.3926760049349705
+        "yaw": -0.5891291228378535,
+        "pitch": 0.07788919934543692,
+        "fov": 1.325599857056214
       },
       "linkHotspots": [
         {
-          "yaw": -0.1173176319198106,
-          "pitch": 0.08710238636571788,
+          "yaw": -2.9144326583292894,
+          "pitch": 0.9665819248491072,
           "rotation": 0,
-          "target": "0-reception-area"
+          "target": "0-safi-street-view"
         },
         {
-          "yaw": 3.121658886438947,
-          "pitch": 0.15650098834117898,
+          "yaw": -1.0855194276252895,
+          "pitch": 0.5754484603651626,
           "rotation": 0,
-          "target": "2-equipment--accessories-section"
+          "target": "2-safi-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-equipment--accessories-section",
-      "name": "Equipment & Accessories Section",
+      "id": "2-safi-2",
+      "name": "Safi 2",
       "levels": [
         {
           "tileSize": 256,
@@ -118,16 +118,556 @@ var APP_DATA = {
       ],
       "faceSize": 2976,
       "initialViewParameters": {
-        "yaw": -2.1602627928155194,
-        "pitch": 0.15638783450744498,
-        "fov": 1.3926760049349705
+        "yaw": 1.761308775733891,
+        "pitch": 0.1678677374409041,
+        "fov": 1.325599857056214
       },
       "linkHotspots": [
         {
-          "yaw": -2.2106395669961802,
-          "pitch": 0.20470058638033706,
+          "yaw": -1.345342715958921,
+          "pitch": 0.5129603653887838,
           "rotation": 0,
-          "target": "1-main-showroom"
+          "target": "1-safi-1"
+        },
+        {
+          "yaw": 1.297784425368592,
+          "pitch": 0.7264365428900508,
+          "rotation": 0,
+          "target": "3-safi-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-safi-3",
+      "name": "Safi 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.178317673622388,
+        "pitch": 0.10123956904903508,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1382556604439316,
+          "pitch": 0.6642461292100261,
+          "rotation": 0,
+          "target": "2-safi-2"
+        },
+        {
+          "yaw": 1.22210539612332,
+          "pitch": 0.7045085629571801,
+          "rotation": 0,
+          "target": "4-safi-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-safi-4",
+      "name": "Safi 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.252894646717543,
+        "pitch": 0.21525978063404239,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.066355009055197,
+          "pitch": 0.5848036117524948,
+          "rotation": 6.283185307179586,
+          "target": "3-safi-3"
+        },
+        {
+          "yaw": -0.9347080221885768,
+          "pitch": 0.6533852865612317,
+          "rotation": 0,
+          "target": "5-safi-5"
+        },
+        {
+          "yaw": -0.0319485448183805,
+          "pitch": 0.6654809260624575,
+          "rotation": 0,
+          "target": "6-safi-6"
+        },
+        {
+          "yaw": 1.8040441261638822,
+          "pitch": 0.7749771529720757,
+          "rotation": 0,
+          "target": "10-safi-10"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-safi-5",
+      "name": "Safi 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.136782622470065,
+        "pitch": 0.2252095940922736,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0171816200043526,
+          "pitch": 0.5696365138382209,
+          "rotation": 0,
+          "target": "4-safi-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-safi-6",
+      "name": "Safi 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.11219973762821311,
+        "pitch": 0.17270842864824054,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.082743960259238,
+          "pitch": 0.7551092429698922,
+          "rotation": 0,
+          "target": "4-safi-4"
+        },
+        {
+          "yaw": -1.447329825059203,
+          "pitch": 0.7331959533433867,
+          "rotation": 0,
+          "target": "7-safi-7"
+        },
+        {
+          "yaw": 0.24660648959119236,
+          "pitch": 0.9081292527666083,
+          "rotation": 0,
+          "target": "8-safi-8"
+        },
+        {
+          "yaw": 1.4407029552831645,
+          "pitch": 0.7819827677348758,
+          "rotation": 0,
+          "target": "9-safi-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-safi-7",
+      "name": "Safi 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.42885280442163776,
+        "pitch": 0.15768652930330518,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0765180404355217,
+          "pitch": 0.7477516353177691,
+          "rotation": 0,
+          "target": "6-safi-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-safi-8",
+      "name": "Safi 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.15390913127323458,
+        "pitch": 0.008236392667086534,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5855283249490615,
+          "pitch": 0.9508386245279361,
+          "rotation": 0,
+          "target": "6-safi-6"
+        },
+        {
+          "yaw": 1.6689310356095826,
+          "pitch": 0.8138516627283483,
+          "rotation": 0,
+          "target": "9-safi-9"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-safi-9",
+      "name": "Safi 9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.07305600541295476,
+        "pitch": 0.3968084985737921,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8892878944221891,
+          "pitch": 0.8794260775686844,
+          "rotation": 0,
+          "target": "8-safi-8"
+        },
+        {
+          "yaw": -3.16559265359,
+          "pitch": 0.3,
+          "rotation": 0,
+          "target": "6-safi-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-safi-10",
+      "name": "Safi 10",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.8124633901367595,
+        "pitch": 0.07336192614720005,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.905220894331399,
+          "pitch": 0.8015854774199305,
+          "rotation": 0,
+          "target": "4-safi-4"
+        },
+        {
+          "yaw": -1.141148714352159,
+          "pitch": 0.648429161068723,
+          "rotation": 0,
+          "target": "11-safi-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-safi-11",
+      "name": "Safi 11",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.8592353749061239,
+        "pitch": 0.14067375349295652,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4800592432181645,
+          "pitch": 0.8089890773890858,
+          "rotation": 0,
+          "target": "10-safi-10"
+        },
+        {
+          "yaw": -0.8292935413701681,
+          "pitch": 0.750861929462376,
+          "rotation": 0,
+          "target": "12-safi-12"
+        },
+        {
+          "yaw": 0.27679109619048603,
+          "pitch": 0.702034817337104,
+          "rotation": 0,
+          "target": "13-safi-13"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-safi-12",
+      "name": "Safi 12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.1351968615308934,
+        "pitch": 0.02274531514465039,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1175265303875754,
+          "pitch": 0.8973659533935798,
+          "rotation": 0,
+          "target": "11-safi-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-safi-13",
+      "name": "Safi 13",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.6347259755340993,
+        "pitch": -0.13067970976862497,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7557061780347816,
+          "pitch": 0.839053329147168,
+          "rotation": 0,
+          "target": "11-safi-11"
         }
       ],
       "infoHotspots": []
@@ -138,6 +678,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
